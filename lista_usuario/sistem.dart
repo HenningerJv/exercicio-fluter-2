c0840005e87639f54170bf2577b0252573f7ad14
HenningerJv <josevictor.henninger@gmail.com>
2024-09-09 15:12:46 -0300

    adicionando a lista de usuarios

---
+++ lista_usuario/sistem.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lista de Usuários',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: UserListScreen(),
+    );
+  }
+}
+
+class UserListScreen extends StatefulWidget {
+  @override
+  _UserListScreenState createState() => _UserListScreenState();
+}
+
+class _UserListScreenState extends State<UserListScreen> {
+  List users = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUsers();
+  }
+
+  Future<void> fetchUsers() async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+    
+    if (response.statusCode == 200) {
+      setState(() {
+        users = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Usuários'),
+      ),
+      body: users.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                final user = users[index];
+                final color = index % 2 == 0 
+                    ? Color(0xFFE3F2FD) 
+                    : Color(0xFFFFF9C4); 
+                
+                return Container(
+                  color: color,
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        user['name'],
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text('Nome de Usuário: ${user['username']}'),
+                      Text('Email: ${user['email']}'),
+                      Text('Telefone: ${user['phone']}'),
+                      Text('Website: ${user['website']}'),
+                      Text('Compania: ${user['company']['name']}'),
+                    ],
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
